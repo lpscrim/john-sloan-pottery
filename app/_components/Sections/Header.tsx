593ed0f41f1d13773f4a8583a62a0bc89d4b6bfe
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-05-11 13:39:30 +0100

    refactor: remove inline styles from Header component images for cleaner code

--- app/_components/Sections/Header.tsx
+++ app/_components/Sections/Header.tsx
@@ -68,7 +68,6 @@ export function Header() {
               alt="John Sloan Pottery"
               width={35}
               height={35}
-              style={{ height: "auto" }}
               className={`transition-all nav-underline duration-500  ${
                 isLight
                   ? "opacity-0 pointer-events-none"
@@ -80,7 +79,6 @@ export function Header() {
               alt="John Sloan Pottery"
               width={35}
               height={35}
-              style={{ height: "auto" }}
               className={`absolute inset-0 transition-all nav-underline duration-500 brightness-200 ${
                 isLight
                   ? "opacity-100 pointer-events-auto"
