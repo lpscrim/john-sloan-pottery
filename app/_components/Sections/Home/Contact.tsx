0262c568e87c2fcfbaa9c86bd4566385eaae8bb3
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-05-23 00:01:59 +0100

    fix: add min-height to Contact section for improved layout

--- app/_components/Sections/Home/Contact.tsx
+++ app/_components/Sections/Home/Contact.tsx
@@ -28,7 +28,7 @@ export function Contact() {
   return (
     <section
       id="contact"
-      className="px-6 py-16 md:py-24 flex items-center"
+      className="min-h-[90svh] px-6 py-16 md:py-24 flex items-center"
     >
       <div className=" mx-auto w-full">
         <div className="mb-16 lg:mb-32 ">
