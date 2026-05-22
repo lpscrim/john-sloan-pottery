699d2445f68d74c819a34ad998ff27ed707bbed7
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-05-22 11:24:50 +0100

    Refactor code structure for improved readability and maintainability

--- app/_components/Sections/Footer.tsx
+++ app/_components/Sections/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-      <footer className="mt-24 py-2 px-6 flex flex-col gap-1 sm:flex-row sm:justify-between sm:items-center text-muted-foreground">
+      <footer className="mt-24 py-4 px-6 flex flex-col gap-1 sm:flex-row sm:justify-between sm:items-center text-muted-foreground">
       <p className="">© {new Date().getFullYear()} JOHN SLOAN POTTERY. ALL RIGHTS RESERVED.</p>
       <p className="text-sm">SITE BY: <Link href="https://www.lpscrim.com" target="_blank" rel="noopener noreferrer" className=" hover:opacity-70 transition-opacity">LEWIS SCRIMGEOUR</Link></p>
     </footer>
