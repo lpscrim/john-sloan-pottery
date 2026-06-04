b9fe2868dc767f294a514b4f9275ebbed231b157
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-06-04 13:43:30 +0100

    fix: adjust padding and layout for improved visual consistency in About section

--- app/_components/Sections/Home/About.tsx
+++ app/_components/Sections/Home/About.tsx
@@ -9,31 +9,45 @@ export async function About() {
   const { text, images } = await getHomeAboutContent();
 
   return (
-    <section id="about" className=" px-4 py-20 xl:py-32 bg-muted/30 items-center flex">
+    <section
+      id="about"
+      className=" px-4 py-24 xl:py-32 bg-muted/30 items-center flex"
+    >
       <div className="w-full mx-auto">
-
         <div className="grid md:grid-cols-2 gap-16 xl:gap-32 px-8 lg:px-16 xl:px-64 mx-auto">
-          <div>
-            <h2 className="text-3xl md:text-4xl tracking-tight text-left mb-8 ">Behind the Kiln</h2>
+          <div className="flex flex-col justify-between h-full">
+            <div>
+            <h2 className="flex-start text-3xl md:text-4xl tracking-tight text-left mb-8 ">
+              Behind the Kiln
+            </h2>
             <div className="space-y-6 text-xl xl:text-2xl">
-              {text.split(/\r?\n\r?\n+/).filter(Boolean).map((para, i) => (
-                <p key={i}>{para}</p>
-              ))}
+              {text
+                .split(/\r?\n\r?\n+/)
+                .filter(Boolean)
+                .map((para, i) => (
+                  <p key={i}>{para}</p>
+                ))}
+            </div>
+            </div>
+            <div className="hidden sm:block sm:mb-8 lg:mb-16 text-center">
+              <Button size="xl">
+                <Link href="/about">In the studio</Link>
+              </Button>
             </div>
           </div>
           <div className="flex justify-end items-center">
             <SwipeGallery
               images={images}
               alt="Artist portrait"
-              className="w-full xl:w-5/6 rounded-sm aspect-4/3 overflow-hidden"
+              className="w-full xl:w-5/6 rounded-sm aspect-4/5 overflow-hidden"
             />
           </div>
         </div>
-         <div className="mt-24 text-center">
-          <Button size="xl">
-            <Link href="/about">In the studio</Link>
-          </Button>
-        </div>
+        <div className="block sm:hidden mt-16 text-center">
+              <Button size="xl">
+                <Link href="/about">In the studio</Link>
+              </Button>
+            </div>
       </div>
     </section>
   );
